bea2ef2db83ca673333fae2416a78805c4148b69
Roton02 <rph645102@gmail.com>
2024-09-28 17:33:04 +0600

    create  review adding system and use react star

--- src/pages/Reviews/Reviews.jsx
+++ src/pages/Reviews/Reviews.jsx
@@ -16,7 +16,7 @@ const Reviews = () => {
     queryKey: ["rating"],
     queryFn: () =>
       axios
-        .get("https://globeglimpse.vercel.app/ClientReview")
+        .get("http://localhost:5000/reviewsCollection")
         .then((res) => res.data),
   });
 
@@ -28,20 +28,18 @@ const Reviews = () => {
     e.preventDefault();
     const form = e.target;
     const ratings = rating;
-    const title = form.fillings.value;
     const description = form.description.value;
     const client_name = user.displayName;
     const image = user.photoURL;
     const addRatings = {
       ratings,
-      title,
       description,
       client_name,
       image,
       dateString,
     };
     axios
-      .post("https://globeglimpse.vercel.app/AddRatings", addRatings)
+      .post("http://localhost:5000/reviews", addRatings)
       .then((res) => {
         if (res.data.insertedId) {
           Swal.fire({
@@ -53,6 +51,7 @@ const Reviews = () => {
           refetch();
         }
       });
+    console.log(addRatings);
   };
   return (
     <div>
@@ -60,38 +59,23 @@ const Reviews = () => {
         text={"Home / Reviews"}
         p={"Read trusted reviews from our customers"}
       ></BannerByAllPage>
-      <section className="bg-white">
-      <div className="max-w-7xl mx-auto">
-      <div className="flex justify-start rounded-xl border-y-2 mt-   bg-gray-100 py-2">
-      <button
-          data-aos="fade-up"
-          data-aos-duration="2000"
-          onClick={() => document.getElementById("my_modal_5").showModal()}
-          className=""
-        >
-          <a
-            href="#_"
-            className="relative border-2  rounded-l-lg  inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-pink-500 transition duration-300 ease-out  border-pink-500 shadow-md group"
-          >
-            <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-pink-500 group-hover:translate-x-0 ease">
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
+      <section className="bg-gray-50">
+        <div className="max-w-7xl mx-auto">
+          <div className="flex justify-start  border-b-2     py-2">
+            <button
+              
+              onClick={() => document.getElementById("my_modal_5").showModal()}
+              className=""
+            >
+              <a
+                href="#_"
+                className="py-3 mb-1 rounded-r-2xl hover:bg-secondary  px-20 hover:cursor-pointer bg-[#E21B70] text-white font-bold "
               >
-                <path d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
-              </svg>
-            </span>
-            <span className="absolute flex items-center justify-center w-full h-full text-pink-500 transition-all duration-300 transform group-hover:translate-x-full ease">
-              Adopt
-            </span>
-            <span className="relative invisible">Adopt</span>
-          </a>
-        </button>
-      </div>
-      </div>
+                Add Your Personal Opinion
+              </a>
+            </button>
+          </div>
+        </div>
 
         <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
           <div className="modal-box relative">
@@ -109,8 +93,20 @@ const Reviews = () => {
                 </div>
               </div>
               <div className="flex flex-col w-full">
-                <input type="text" className="input input-bordered w-full max-w-2xl mb-2" name="" value={user?.displayName} id="" />
-                <input type="text" className="input input-bordered w-full max-w-2xl mb-2" name="" value={user?.email} id="" />
+                <input
+                  type="text"
+                  className="input input-bordered w-full max-w-2xl mb-2"
+                  name=""
+                  value={user?.displayName}
+                  id=""
+                />
+                <input
+                  type="text"
+                  className="input input-bordered w-full max-w-2xl mb-2"
+                  name=""
+                  value={user?.email}
+                  id=""
+                />
                 <textarea
                   required
                   rows="3"
@@ -691,71 +687,74 @@ const Reviews = () => {
               </blockquote>
             </div>
 
-            <div className="mb-8 sm:break-inside-avoid">
-              <blockquote className="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
-                <div className="flex items-center gap-4">
-                  <img
-                    alt=""
-                    src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
-                    className="size-14 rounded-full object-cover"
-                  />
-
-                  <div>
-                    <div className="flex justify-center gap-0.5 text-green-500">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="size-5"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                      </svg>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="size-5"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                      </svg>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="size-5"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                      </svg>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="size-5"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                      </svg>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="size-5"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                      </svg>
+            {
+              data.map((review , i)=> (
+                <div key={i} className="mb-8 sm:break-inside-avoid">
+                <blockquote className="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
+                  <div className="flex items-center gap-4">
+                    <img
+                      alt=""
+                      src={review.image}
+                      className="size-14 rounded-full object-cover"
+                    />
+  
+                    <div>
+                      <div className="flex justify-center gap-0.5 text-green-500">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="size-5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                        </svg>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="size-5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                        </svg>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="size-5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                        </svg>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="size-5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                        </svg>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="size-5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                        </svg>
+                      </div>
+  
+                      <p className="mt-0.5 text-lg font-medium text-gray-900">
+                      {review.client_name}
+                      </p>
                     </div>
-
-                    <p className="mt-0.5 text-lg font-medium text-gray-900">
-                      Paul Starr
-                    </p>
                   </div>
-                </div>
-
-                <p className="mt-4 text-gray-700">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-                  numquam, unde molestiae commodi temporibus dicta.
-                </p>
-              </blockquote>
-            </div>
+  
+                  <p className="mt-4 text-gray-700">
+                    {review.description}
+                  </p>
+                </blockquote>
+              </div>
+              ))
+            }
           </div>
         </div>
       </section>
