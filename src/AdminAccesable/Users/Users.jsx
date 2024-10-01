6fe80092ae2784a07ccd72466ee61ee31800a299
Roton02 <rph645102@gmail.com>
2024-10-01 23:46:07 +0600

    data refache using tanstackquery

--- src/AdminAccesable/Users/Users.jsx
+++ src/AdminAccesable/Users/Users.jsx
@@ -1,14 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 
 const Users = () => {
-  const [users , setUsers] = useState([])
-  useEffect(() => {
-    axios
-      .get("http://localhost:5000/Admin/user")
-      .then((res) => setUsers(res.data));
-  }, []);
+  
+  const {data: users = [],  refetch} = useQuery({
+    queryKey: ["user"],
+    queryFn: async()=>{
+    const {data} = await axios.get("http://localhost:5000/Admin/user")
+    return data;
+    },
+  });
+
   console.log(users);
+  const handleMakeAdmin = (id) => {
+    axios.patch(`/users/admin/${id}`).then((res) => {
+      console.log(res.data);
+      if (res.data.modifiedCount > 0) {
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `You Achive Admin Role !`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
+  };
   return (
     <div className="max-w-6xl mx-auto mt-10">
       <div className="relative overflow-x-auto  ">
@@ -30,49 +49,50 @@ const Users = () => {
               <th scope="col" className="px-6 py-3">
                 Role
               </th>
-              
+
               <th scope="col" className="px-6 py-3">
                 Action
               </th>
             </tr>
           </thead>
           <tbody>
-           {
-            users.map((user , i ) => (
+            {users.map((user, i) => (
               <tr key={i} className="b ">
-              <th
-                scope="row"
-                className="px-6 py-4   whitespace-nowrap "
-              >
-                {i+1}
-              </th>
-              <th
-                scope="row"
-                className="px-3 py-4 font-medium  whitespace-nowrap "
-              >
-                <img className="w-16  object-cover rounded-full" src={user.image} alt="" />
-              </th>
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium  whitespace-nowrap "
-              >
-                {user.name}
-              </th>
-              
-              <td className=" py-4 ">{user.email}</td>
-              <td className="px-6 py-4">Admin</td>
-              <td className="px-6 py-4">
-                <a
-                  href="#"
-                  className="font-medium bg-green-200 px-2 rounded-lg text-blue-600 dark:text-blue-500 hover:underline"
+                <th scope="row" className="px-6 py-4   whitespace-nowrap ">
+                  {i + 1}
+                </th>
+                <th
+                  scope="row"
+                  className="px-3 py-4 font-medium  whitespace-nowrap "
                 >
-                  Make Admin
-                </a>
-              </td>
-            </tr>
-            ))
-           }
-            
+                  <img
+                    className="w-16  object-cover rounded-full"
+                    src={user.image}
+                    alt=""
+                  />
+                </th>
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium  whitespace-nowrap "
+                >
+                  {user.name}
+                </th>
+
+                <td className=" py-4 ">{user.email}</td>
+                <td className="px-6 py-4">Admin</td>
+                <td className="px-6 py-4">
+                  <button
+                    onClick={() => {
+                      handleMakeAdmin(user._id);
+                    }}
+                    href="#"
+                    className="font-medium bg-green-200 px-2 rounded-lg text-blue-600 dark:text-blue-500 hover:underline"
+                  >
+                    Make Admin
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
