ee6c6f65a8883e013dd3bad6595ee39e7c18504a
Roton02 <rph645102@gmail.com>
2024-10-01 23:18:28 +0600

    show user data in ui dynamically  using asios faching tools

--- src/AdminAccesable/Users/Users.jsx
+++ src/AdminAccesable/Users/Users.jsx
@@ -1,85 +1,78 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Users = () => {
+  const [users , setUsers] = useState([])
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/Admin/user")
+      .then((res) => setUsers(res.data));
+  }, []);
+  console.log(users);
   return (
     <div className="max-w-6xl mx-auto mt-10">
       <div className="relative overflow-x-auto  ">
         <table className="w-full text-sm text-left rtl:text-right ">
-          <thead className="text-xs  uppercase ">
+          <thead className="text-xs bg-slate-200  uppercase ">
             <tr>
               <th scope="col" className="px-6 py-3">
-                Product name
+                No
               </th>
               <th scope="col" className="px-6 py-3">
-                Color
+                Profile
               </th>
               <th scope="col" className="px-6 py-3">
-                Category
+                User name
               </th>
               <th scope="col" className="px-6 py-3">
-                Price
+                Gmail
               </th>
+              <th scope="col" className="px-6 py-3">
+                Role
+              </th>
+              
               <th scope="col" className="px-6 py-3">
                 Action
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr className="b ">
+           {
+            users.map((user , i ) => (
+              <tr key={i} className="b ">
+              <th
+                scope="row"
+                className="px-6 py-4   whitespace-nowrap "
+              >
+                {i+1}
+              </th>
+              <th
+                scope="row"
+                className="px-3 py-4 font-medium  whitespace-nowrap "
+              >
+                <img className="w-16  object-cover rounded-full" src={user.image} alt="" />
+              </th>
               <th
                 scope="row"
                 className="px-6 py-4 font-medium  whitespace-nowrap "
               >
-                Apple MacBook Pro 17
+                {user.name}
               </th>
-              <td className="px-6 py-4">Silver</td>
-              <td className="px-6 py-4">Laptop</td>
-              <td className="px-6 py-4">$2999</td>
+              
+              <td className=" py-4 ">{user.email}</td>
+              <td className="px-6 py-4">Admin</td>
               <td className="px-6 py-4">
                 <a
                   href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  className="font-medium bg-green-200 px-2 rounded-lg text-blue-600 dark:text-blue-500 hover:underline"
                 >
-                  Edit
+                  Make Admin
                 </a>
               </td>
             </tr>
-            <tr className="bg-white border-b ">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium  whitespace-nowrap "
-              >
-                Microsoft Surface Pro
-              </th>
-              <td className="px-6 py-4">White</td>
-              <td className="px-6 py-4">Laptop PC</td>
-              <td className="px-6 py-4">$1999</td>
-              <td className="px-6 py-4">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
-                </a>
-              </td>
-            </tr>
-            <tr className="">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium  whitespace-nowrap "
-              >
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">Black</td>
-              <td className="px-6 py-4">Accessories</td>
-              <td className="px-6 py-4">$99</td>
-              <td className="px-6 py-4">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
-                </a>
-              </td>
-            </tr>
+            ))
+           }
+            
           </tbody>
         </table>
       </div>
