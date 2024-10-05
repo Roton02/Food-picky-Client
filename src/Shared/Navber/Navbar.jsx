642b198cbae3d821574768a932a24caa326454e0
Roton02 <rph645102@gmail.com>
2024-10-05 09:49:39 +0600

    add new page for normal user he see his own order

--- src/Shared/Navber/Navbar.jsx
+++ src/Shared/Navber/Navbar.jsx
@@ -1,6 +1,6 @@
 import "./Navbar.css";
 import "animate.css";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../ContextProvider/ContextProvider";
 import { Link, NavLink } from "react-router-dom";
 import { FaCartArrowDown } from "react-icons/fa";
@@ -14,30 +14,6 @@ const Navbar = () => {
   const { Logout, user } = useContext(AuthContext);
 
   console.log(user);
-  const [theme, setTheme] = useState(() => {
-    // Retrieve theme from localStorage on component mount
-    const locatTheme = localStorage.getItem("theme");
-    // If no theme is found in localStorage, default to dark theme
-    // return locatTheme === "dark" ? true : false;
-    return locatTheme === "dark" ? false : true;
-  });
-
-  // Function to toggle theme
-  const toggleTheme = () => {
-    setTheme((prevTheme) => !prevTheme);
-  };
-
-  useEffect(() => {
-    // Store current theme in localStorage
-    // localStorage.setItem("theme", theme ? "dark" : "light");
-    localStorage.setItem("theme", theme ? "light" : "dark");
-
-    // Apply theme to HTML element
-    // document.querySelector('html').setAttribute('data-theme', theme ? "dark" : "light");
-    document
-      .querySelector("html")
-      .setAttribute("data-theme", theme ? "dark" : "light");
-  }, [theme]); // Re-run effect when theme changes
 
   return (
     <div className=" w-full   bg-white shadow-xl  ">
@@ -444,50 +420,13 @@ const Navbar = () => {
                     }
                     to="/AllFoodRequest"
                   >
-                    All Order 
+                    All Order
                   </NavLink>
                 </>
               )}
             </div>
           </div>
           <div className="navbar-end ">
-            <label className="mr-5 cursor-pointer grid place-items-center">
-              <input
-                type="checkbox"
-                onClick={toggleTheme}
-                checked={theme}
-                className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
-              />
-              <svg
-                className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
-                xmlns="http://www.w3.org/2000/svg"
-                width="14"
-                height="14"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <circle cx="12" cy="12" r="5" />
-                <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
-              </svg>
-              <svg
-                className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
-                xmlns="http://www.w3.org/2000/svg"
-                width="14"
-                height="14"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-              </svg>
-            </label>
             {user ? (
               <div className="flex items-center ">
                 <div className="dropdown dropdown-end">
@@ -513,10 +452,8 @@ const Navbar = () => {
                         alt=""
                       />
                     </div>
-                    <li className="  rounded-lg bg-black text-white font-bold  text-start "> 
-                     <span>
-                     {user?.displayName} 
-                     </span>
+                    <li className="  rounded-lg bg-black text-white font-bold  text-start ">
+                      <span>{user?.displayName}</span>
                     </li>
                     <li>
                       <NavLink
@@ -526,7 +463,7 @@ const Navbar = () => {
                         Profile
                       </NavLink>
                     </li>
-                    
+
                     <li className="pb-6 mt-3">
                       <button
                         onClick={Logout}
